b51040900f59f077f628ed29cff88e4ca60c8d8b
Foroni1 <foronialessio@gmail.com>
2020-04-06 23:35:49 +0200

    xxx

--- Tutorato/src/base/TestCondiv.java
+++ Tutorato/src/base/TestCondiv.java
@@ -3,11 +3,9 @@ package base;
 public class TestCondiv {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Hello Gnoatto bitch");
-=======
 		System.out.println("Hello Gnoatto22222222222 333333333");
->>>>>>> branch 'master' of https://github.com/Foroni1/Test.git
 	}
 
 }
