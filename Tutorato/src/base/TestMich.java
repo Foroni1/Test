8eaf4f0ae1b43b67c0cb10f8a7892572bd6f512d
mich19977 <micheledelmoro97@gmail.com>
2020-04-06 23:58:43 +0200

    mich

---
+++ Tutorato/src/base/TestMich.java
@@ -0,0 +1,18 @@
+package base;
+
+public class TestMich {
+
+	public static void main(String[] args) {
+		/*speriamo 
+		 * che
+		 * questo sistema
+		 * funzioni
+		 */
+		/*speriamo 
+		 * che
+		 * questo sistema
+		 * funzioni
+		 */
+	}
+
+}
