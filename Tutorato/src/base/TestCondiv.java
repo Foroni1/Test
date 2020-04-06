861b78fb3481209f5aac46723890b76c4cd222c3
ubertotedoldi <ubertotedoldi@air-di-uberto.homenet.telecomitalia.it>
2020-04-06 23:28:10 +0200

    o

--- Tutorato/src/base/TestCondiv.java
+++ Tutorato/src/base/TestCondiv.java
@@ -3,7 +3,7 @@ package base;
 public class TestCondiv {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Gnoatto22222222222");
+		System.out.println("Hello Gnoatto22222222222 333333333");
 	}
 
 }
