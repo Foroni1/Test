de9bb0b6e074d8dd8f0a0a9aa3ce4a41dbc0489e
Matteo <matteo.perin@yahoo.it>
2020-04-06 23:26:10 +0200

    gugu

--- Tutorato/src/base/TestCondiv.java
+++ Tutorato/src/base/TestCondiv.java
@@ -3,7 +3,7 @@ package base;
 public class TestCondiv {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Gnoatto");
+		System.out.println("Hello Gnoatto22222222222");
 	}
 
 }
